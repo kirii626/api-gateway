ce07a43e2991b8eea586bf61531b05245f6c10fa
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-04-30 11:34:01 -0300

    declare new paths using the JwtAuthenticationFilter

--- src/main/java/com/accenture/api_gateway/config/GatewayConfig.java
+++ src/main/java/com/accenture/api_gateway/config/GatewayConfig.java
@@ -8,15 +8,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public GatewayConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
-                .route("sale-point-service", r -> r.path("/api/sale-point/**")
+                .route("admin-sale-point-service", r -> r.path("/api/admin-sale-point/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
                         .uri("lb://sale-point-service"))
-                .route("products-service", r -> r.path("/api/costs/**")
+                .route("admin-cost-service", r -> r.path("/api/admin-cost/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
                         .uri("lb://sale-point-service"))
-                .route("accreditations-service", r -> r.path("/api/accreditations/**")
+                .route("admin-accreditation-service", r -> r.path("/api/admin-accreditation/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
                         .uri("lb://accreditation-service"))
+                .route("user-accreditation-service", r -> r.path("/api/user-accreditation/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
+                        .uri("lb://accreditation-service"))
+                .route("user-service", r -> r.path("/api/user/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
+                        .uri("lb://user-microservice"))
+                .route("admin-service", r -> r.path("/api/admin/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
+                        .uri("lb://user-microservice"))
+                .route("auth-service", r -> r.path("/api/auth/**")
+                        .filters(f -> f.filter(jwtAuthenticationFilter))
+                        .uri("lb://user-microservice"))
                 .build();
     }
 }
